eef22f6359175b0a058f3b96d8b5b104d52b9ecf
macewiczmaciej <macewicz.maciej@gmail.com>
2021-06-13 21:26:44 +0200

    poprawnione dodawanie zamowienia

--- main.py
+++ main.py
@@ -1,60 +1,83 @@
+import datetime
+
 import obliczenia
 import random
 
-menu = {'własna':[15,20],'margarita':[20,30],'hawaii':[23,35],'salami':[22,34],'carbonara':[21,32]}
-skladniki = {'szynka':4,'ser':3,'salami':4,'bekon':3,'kurczak':5,'pieczarki':2,'ananas':3,'cebula':2}
-sosy = ['bez sosu','czosnkowy','pomidorowy','miodowo-musztardowy','majonezowy']
+menu = {'własna': [15, 20], 'margarita': [20, 30], 'hawaii': [23, 35], 'salami': [22, 34], 'carbonara': [21, 32]}
+skladniki = {'szynka': 4, 'ser': 3, 'salami': 4, 'bekon': 3, 'kurczak': 5, 'pieczarki': 2, 'ananas': 3, 'cebula': 2}
 zamowienie = []
-cena = 0
-sos = None
+cena = []
 
 def showMenu():
     print(dash)
     print('{:^24}'.format('MENU'))
     print(dash)
     for i in range(len(menu)):
-        print('{:<1}{:<2}{:<12}{:>4}{:>4}'.format(i + 1, '.', list(menu.keys())[i].capitalize(), list(menu.values())[i][0],list(menu.values())[i][1]))
+        print('{:<1}{:<2}{:<12}{:>4}{:>4}'.format(i + 1, '.', list(menu.keys())[i].capitalize(),list(menu.values())[i][0], list(menu.values())[i][1]))
     print(dash)
+
+def zamowPizze():
+    print("Podaj numer pizzy którą chcesz zamówić")
+    choicePizza = input("Wybieram pizzę: ")
+
+    # wlasna
+    if choicePizza == "1":
+        wybraneSkladniki = []
+        choiceSkladnik = ""
+        wartoscSkladnikow = 0
+        showSkladniki()
+        while (choiceSkladnik != 'gotowe'):
+            print("Podaj numer składnika który chcesz dodać do pizzy")
+            print("Wpisz 'gotowe' żeby przejść dalej")
+            choiceSkladnik = input("Wybieram składnik: ")
+            if choiceSkladnik == 'gotowe':
+                break
+            else:
+                wybraneSkladniki.append(list(skladniki.keys())[int(choiceSkladnik) - 1])
+                wartoscSkladnikow += list(skladniki.values())[int(choiceSkladnik) - 1]
+                print("Wybrane składniki: ", wybraneSkladniki)
+        sumujZamowienie(choicePizza, wyborRozmiaru(choicePizza),wartoscSkladnikow)
+    else:
+        sumujZamowienie(choicePizza, wyborRozmiaru(choicePizza))
+
+def wyborRozmiaru(choicePizza):
+    print("Jaki rozmiar pizzy? Podaj 'mala' lub 'duza'")
+    choiceRozmiar = input("Wybieram rozmiar: ")
+    if choiceRozmiar == 'mala':
+        zamowienie.append(list(menu.keys())[int(choicePizza) - 1])
+    elif choiceRozmiar == 'duza':
+        zamowienie.append(list(menu.keys())[int(choicePizza) - 1])
+    return choiceRozmiar
+
 
 def showSkladniki():
     print(dash)
     print('{:^24}'.format('SKŁADNIKI'))
     print(dash)
     for i in range(len(skladniki)):
-        print('{:<1}{:<2}{:<12}{:>4}'.format(i + 1, '.', list(skladniki.keys())[i].capitalize(), list(skladniki.values())[i]),'PLN')
+        print('{:<1}{:<2}{:<12}{:>4}'.format(i + 1, '.', list(skladniki.keys())[i].capitalize(),
+                                            list(skladniki.values())[i]), 'PLN')
     print(dash)
 
-def wyborSosu():
-    print(dash)
-    print('{:^24}'.format('SOSY'))
-    print(dash)
-    for i in range(len(sosy)):
-        print('{:<1}{:<2}{:<1}'.format(i, '.', sosy[i]))
-    print(dash)
-    print("Jaki sos do pizzy? ")
-    choiceSos = int(input("Wybieram: "))
-    sos = sosy[choiceSos]
-    print("Wybrany sos:", sos)
 
-def sumujZamowienie():
-    suma = cena
-    if sos != 0 and sos != None:
-        suma+=3
-    print('Wartość zamówienia wynosi: ',suma,'PLN')
+def sumujZamowienie(choicePizza, choiceRozmiar, wartoscSkladnikow = 0):
+    if (choiceRozmiar == 'mala'):
+        cena.append(list(menu.values())[int(choicePizza) - 1][0] + wartoscSkladnikow)
+    elif (choiceRozmiar == 'duza'):
+        cena.append(list(menu.values())[int(choicePizza) - 1][1] + wartoscSkladnikow)
 
 def szczegolyZamowienia():
-
     print(dash)
     print('{:^24}'.format("MOJE ZAMÓWIENIE"))
     print(dash)
-    if len(zamowienie)==0:
+    if len(zamowienie) == 0:
         print("Jeszcze nic nie wybrałeś :)")
     else:
-        print("Twoje zamówienie: ")
-        print(zamowienie)
-        sumujZamowienie()
-
-dash = '-'*24
+        for i in range(len(zamowienie)):
+            print('{:<1}{:<2}{:<12}{:<4}'.format(i + 1, '.', zamowienie[i], cena[i]))
+        print()
+        print("Łączna wartość zamówienia to:", sum(cena))
+dash = '-' * 24
 print("Witaj w Pizzeri u Jarosława")
 choiceMenu = 2
 while choiceMenu != 0:
@@ -67,61 +90,22 @@ while choiceMenu != 0:
     """)
     choiceMenu = int(input("Wybieram: "))
 
-    #wyjdz z menu
-    if(choiceMenu==0):
+    # wyjdz z menu
+    if (choiceMenu == 0):
         print("Do zobaczenia :)")
 
-    #zamow pizze
-    if(choiceMenu==1):
+    # zamow pizze
+    if (choiceMenu == 1):
         showMenu()
-        print("Podaj numer pizzy którą chcesz zamówić")
-        choicePizza = input("Wybieram: ")
-
-        #wlasna
-        if choicePizza == "1":
-            wybraneSkladniki = []
-            choiceSkladnik = None;
-            showSkladniki()
-            while(choiceSkladnik != 'dalej'):
-                print("Podaj numer składnika który chcesz dodać do pizzy")
-                print("Wpisz 'dalej', jeśli chcesz przejść do kolejnego kroku lub 'anuluj', żeby powrócić do MENU")
-                choiceSkladnik = input("Wybieram: ")
-                if choiceSkladnik == 'anuluj':
-                    break
-                elif choiceSkladnik == 'dalej':
-                    print("Jaki rozmiar pizzy? Podaj 'mala' lub 'duza'")
-                    choiceRozmiar = input("Wybieram: ")
-                    if choiceRozmiar == 'mala':
-                        zamowienie.append(list(menu.keys())[int(choicePizza)-1])
-                        cena += list(menu.values())[int(choicePizza)-1][0]
-                    elif choiceRozmiar == 'duza':
-                        zamowienie.append(list(menu.keys())[int(choicePizza)-1])
-                        cena += list(menu.values())[int(choicePizza)-1][1]
-                    wyborSosu()
-
-                else:
-                    wybraneSkladniki.append(list(skladniki.keys())[int(choiceSkladnik)-1])
-                    cena += list(skladniki.values())[int(choiceSkladnik)-1
-                    ]
-                    print("Wybrane składniki: ",wybraneSkladniki)
-        else:
-            print("Jaki rozmiar pizzy? Podaj 'mala' lub 'duza'")
-            choiceRozmiar = input("Wybieram: ")
-            if choiceRozmiar == 'mala':
-                zamowienie.append(list(menu.keys())[int(choicePizza)-1])
-                cena += list(menu.values())[int(choicePizza)-1][0]
-            elif choiceRozmiar == 'duza':
-                zamowienie.append(list(menu.keys())[int(choicePizza)-1])
-                cena += list(menu.values())[int(choicePizza)-1][1]
-            wyborSosu()
+        zamowPizze()
         szczegolyZamowienia()
 
-    #pokaz menu
-    if(choiceMenu==2):
+    # pokaz menu
+    if (choiceMenu == 2):
         showMenu()
 
-    #moje zamowienie
-    if(choiceMenu==3):
+    # moje zamowienie
+    if (choiceMenu == 3):
         szczegolyZamowienia()
         print()
         print("1. Zamów")
@@ -132,18 +116,26 @@ while choiceMenu != 0:
         if choiceZamowienie == 0:
             continue
         elif choiceZamowienie == 1:
+            with open("zamowienia.txt","a") as file:
+                file.write(str(datetime.datetime.now()))
+                file.write("\n")
+                file.write(str(zamowienie))
+                file.write("\n")
+
             print(dash)
             print("SUKCES! Zamówiono wybrane pizze :)")
             print(dash)
+            cena.clear()
             zamowienie.clear()
         elif choiceZamowienie == 2:
-            zamowienie.clear()
             print(dash)
             print("Zamówienie zostało anulowane.")
             print(dash)
+            zamowienie.clear()
+            cena.clear()
 
-    #kontakt
-    if(choiceMenu==4):
+    # kontakt
+    if (choiceMenu == 4):
         print(dash)
         print('{:^22}'.format('KONTAKT'))
         print(dash)
---
+++ obliczenia.py
@@ -0,0 +1,6 @@
+b = 2
+if b == 0:
+    exit()
+else:
+    a = 12
+    print(a)
